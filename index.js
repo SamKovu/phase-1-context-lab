774fff0d8e3aa6f0d1d797484097e53f5028aff9
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-04-05 22:52:21 +0300

    Completed lab

--- index.js
+++ index.js
@@ -18,6 +18,114 @@ const allWagesFor = function () {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+    console.log(payable);
+    return payable;
 }
 
+function createEmployeeRecord(input){
+
+    let employee = {
+        firstName : input[0],
+        familyName : input[1],
+        title: input[2],
+        payPerHour: input[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+
+    return employee;
+}
+
+function createEmployeeRecords(inputArray){
+      
+    let groupArray=[];
+    for(let element of inputArray){
+     groupArray.push(createEmployeeRecord(element));
+    }
+ 
+    return groupArray;
+ 
+ }
+
+ function createTimeInEvent(date){
+    
+    let timeIn={
+        type: "TimeIn",
+        hour: parseInt(date.slice(-4)),
+        date: date.slice(0,10),
+      }
+      
+      this["timeInEvents"].push(timeIn);
+      return this;
+
+ }
+
+ function createTimeOutEvent(date){
+
+    let timeOut={
+      type: "TimeOut",
+      hour: parseInt(date.slice(-4)),
+      date: date.slice(0,10),
+    }
+    
+    this["timeOutEvents"].push(timeOut);
+    return this;   
+}
+
+function hoursWorkedOnDate(date){
+    
+    let timeIn;
+    let timeOut;
+
+    this.timeInEvents.forEach(element => {
+        if(element.date===date.slice(0,10)){
+            timeIn=element.hour/100;
+            //timeIn= parseInt(element.hour)/100;
+           // timeIn= Math.trunc(timeIn);
+        }
+        
+    });
+
+    this.timeOutEvents.forEach(element => {
+        if(element.date===date.slice(0,10)){
+            timeOut=element.hour/100;
+         // timeOut= parseInt(element.hour)/100;
+        // timeOut= Math.trunc(timeOut);
+        }
+        
+    });
+
+    return  timeOut-timeIn;
+}
+
+function wagesEarnedOnDate(date){
+
+    let totalTime=hoursWorkedOnDate.call(this,date);
+    let wage=this.payPerHour*totalTime;
+    return wage;
+
+}
+
+
+function findEmployeeByFirstName(records,firstName){
+    for(let record of records){
+        if(record.firstName===firstName){
+            return record;
+        }
+    }
+}
+
+
+
+
+function calculatePayroll(records){
+
+    let totalPayroll=0;
+    for(let record of records){
+    totalPayroll+=allWagesFor.call(record);
+    
+    }
+    console.log(totalPayroll);
+    return totalPayroll;
+ 
+ }
